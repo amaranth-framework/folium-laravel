393d8fd48d11406e42ed4d43f44509eff7594325
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2018-10-12 17:47:56 +0300

    almost there with Find trait for Laravel

--- src/Folium/Rest/Utils.php
+++ src/Folium/Rest/Utils.php
@@ -4,21 +4,42 @@ namespace Itmcdev\Folium\Rest;
 
 trait Utils
 {
+
+    static $operands = [
+        '$eq' => '=',
+        '$lt' => '<',
+        '$lte' => '<=',
+        '$gt' => '>',
+        '$gte' => '>',
+        '$ne' => '!=',
+        '$like' => 'LIKE',
+    ];
+
+    /**
+     * @param array $array
+     * @return bool
+     */
     protected function isAssoc(array $array) {
         return array_keys($arr) !== range(0, count($arr) - 1);
     }
 
-    protected function isLaravelModel($class) {
-        if (!class_exists(\Illuminate\Database\Eloquent\Model)) {
-            return false;
+    /**
+     * @param array $filters
+     */
+    protected function parseFilters(array $filters)
+    {
+        $parsedFilters = [];
+        foreach ($filters as $filterKey => $filterValue) {
+            if (preg_match('/([ˆ\[]+)\[([ˆ\]]+)\]/', $filter, $matches)) {
+                if (defined(self::$operands, $matches[2])) {
+                    $parsedFilters[] = [ $matches[1], $matches[2], $filterValue ];
+                } else {
+                    throw new UnknownOperand($matches[2]);
+                }
+            } else {
+                $parsedFilters[] = [ $filterKey, $filterValue ];
+            }
         }
-        if (!is_subclass_of($class, \Illuminate\Database\Eloquent\Model)) {
-            return false;
-        }
-        return true;
-    }
-
-    protected function isDoctrineModel($class) {
-        // TODO: ...
+        return $parsedFilters;
     }
 }
